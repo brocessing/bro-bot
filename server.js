7acba87a579fa2f0e1a9538c53bebea1e3c38051
Léo Ternoir <leo@leoternoir.com>
2016-05-01 00:52:28 +0200

    Add message formatting and communications

--- server.js
+++ server.js
@@ -2,28 +2,12 @@
  * Require and handle the Bro instance
  */
 
-var Log = require('./lib/helpers');
-var log = new Log('Server');
+var Log = require('./lib/logger');
+var Bro = require('./lib/client');
 
 // Get the token
 var TOKEN = process.env.SLACK_API_TOKEN || '';
 
-
-var Bro = require('./lib/client');
+// Initialize all the things
+var log = new Log('Server');
 var bro = new Bro(TOKEN);
-
-// ON command
-bro.on('command', function(req, res) {
-  var cmd = req.command;
-
-  log.success('Command received : %s', cmd);
-
-  if(cmd === '/vote') {
-    // Handle the vote command
-    bro.post('Wow this ' + req.user_name + ' fucker casted a vote!', req.channel_id);
-    res.send({});
-  } else {
-    // Handle the other commands
-    response.send('I don\'t know this bullshit command, bro!');
-  }
-});
