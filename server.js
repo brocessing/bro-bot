8c5945326aa7850d5bffb8d3637f14813a9f4328
Léo Ternoir <leo@leoternoir.com>
2016-04-30 16:54:57 +0200

    Bro now reacts to slash commands
    Removed some useless functions

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 /**
- *
- * Create the server and handle actions
+ * Require and handle the Bro instance
  */
 
-// Require modules
-var express = require('express');
-var bodyParser = require('body-parser');
+var Log = require('./lib/helpers');
+var log = new Log('Server');
 
-// Create server
-var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Get the token
+var TOKEN = process.env.SLACK_API_TOKEN || '';
 
-// Listen to requests
-var server = app.listen(8888);
+
+var Bro = require('./lib/client');
+var bro = new Bro(TOKEN);
+
+// ON command
+bro.on('command', function(req, res) {
+  var cmd = req.command;
+
+  log.success('Command received : %s', cmd);
+
+  if(cmd === '/vote') {
+    // Handle the vote command
+    bro.post('Wow this ' + req.user_name + ' fucker casted a vote!', req.channel_id);
+    res.send({});
+  } else {
+    // Handle the other commands
+    response.send('I don\'t know this bullshit command, bro!');
+  }
+});
